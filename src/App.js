4b7eaebc3787e61ec13fdb0c7fa46a8f97e94996
arshidte <arshiddiyan.te.adte@gmail.com>
2022-01-02 15:39:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [toDos,setToDos] = useState([]);
+  const [toDo,setToDo] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>To-Do to Today 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now(), text: toDo, status: false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {toDos.map((value)=>{
+        return (<div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              setToDos(toDos.filter(obj=>{
+                if(obj.id===value.id){
+                  obj.status=e.target.checked
+                  console.log(obj);
+                  console.log(value);
+                  console.log(e.target.checked);
+                }
+                return obj;
+              }))
+            }} value={value.status} type="checkbox" name="" id="" />
+            <p>{value.text}</p>
+          </div>
+          <div className="right">
+            <i onClick={(e)=>{
+              setToDos(toDos.filter(obj=>{
+                let temp;
+                if(obj.id !== value.id){
+                  temp = obj;
+                }
+                return temp;
+              }))
+            }} className="fas fa-times"></i>
+          </div>
+        </div>
+        )})}
+
+      </div>
     </div>
   );
 }
